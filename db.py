c8e77142c2fa6d7b6d782778e9d4dc37676e89c9
madolu2 <unclemadolu2@gmail.com>
2020-07-20 12:08:20 +0700

    Create - Created database pony.orm file with entity Valutes. Db - sqlite3. Db name Valutes.db

---
+++ db.py
@@ -0,0 +1,15 @@
+from pony.orm import PrimaryKey, Required, Optional, Database, commit, set_sql_debug, db_session, select
+
+settings = dict(provider='sqlite', filename='valutes.db', create_db=True)
+db = Database(**settings)
+set_sql_debug(True)
+
+class Valutes(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    char_code = Required(str,unique=True)
+    to_usd = Optional(float)
+    to_eur = Optional(float)
+    to_cny = Optional(float)
+    to_jpy = Optional(float)
+
+
