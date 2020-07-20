2453d355aed04b3fad81f7c361763ce0f126724f
madolu2 <unclemadolu2@gmail.com>
2020-07-20 11:35:04 +0700

    Add - Added exchange func (convert val to val)

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ response = requests.get(url).json()
 # destructuring list to vars
 dollar, euro, yuan, jpy = [response['Valute'][key]['Previous']
                            for key in char_codes]
+# не понимаю зачем нужно делить йены на 100, 
+# но без этого формула ломается
+jpy /= 100
 
-
-
+# divide 1 by valute = opposite course
+exchange_valute = lambda val1, val2: \
+                    round((1 / val2) / (1 / val1), 3) 
